befb67883a8603991ec3ce6d4e0827ce6ef4be0f
bajtek94 <damimiko94@gmail.com>
2017-04-09 16:56:37 +0200

    chyba gotowe

---
+++ Tablice/Tablice/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tablice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyArray tab = new MyArray();
+            //int a = 5;
+            //a = tab[6];
+            tab.Add(1);
+            tab.Add(2);
+            tab.Add(5);
+            tab[634] = 43;
+            //Console.WriteLine(a);
+        }
+
+
+
+    }
+}
---
+++ Tablice/Tablice/MyArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablice
+{
+    class MyArray
+    {
+        int[] tab;
+        int countOfElements;
+
+        public MyArray()
+        {
+            countOfElements = 0;
+            tab = new int[4];
+        }
+
+        public int this[int el]
+        {
+            get
+            {
+                try
+                {
+                    return tab[el];
+                }catch(Exception ex)
+                {
+                    Console.WriteLine("Error ! ! !");
+                }
+                return 0;
+            }
+            set
+            {
+                if (el >= countOfElements)
+                {
+                    growTab(el+1);
+                    countOfElements = el+1;
+                } 
+                tab[el] = value;
+            }
+        }
+
+        public void Add(int el)
+        {
+            int count = tab.Count();
+            if (countOfElements >= count)
+            {
+                growTab(2 * count);
+            }
+            //Console.WriteLine("Rozmiar: " + count)
+            tab[countOfElements] = el;
+            countOfElements++;
+        }
+
+        private void growTab(int newSize)
+        {
+            int count = tab.Count();
+            Console.WriteLine("Stary rozmiar: " + count);
+            Array.Resize(ref tab, newSize);
+            Console.WriteLine("Zwiększono rozmiar do: " + tab.Count());
+            //tab = new int[count * 2];
+        }
+    }
+}
